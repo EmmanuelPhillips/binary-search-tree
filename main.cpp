d71fd11b6e1c865f9da07bd2246dc6eee09ea49d
EmmanuelPhillips <emmanuelphillips33@gmail.com>
2026-06-01 22:04:14 +0100

    Created remove function to remove nodes from BST based on value

--- main.cpp
+++ main.cpp
@@ -64,6 +64,72 @@ public:
       }
     }
   }
+
+  void remove(int value) {
+    if (m_root == nullptr) {
+      std::cout << "No nodes to remove.\n";
+      return;
+    }
+    Node *previous = nullptr;
+    Node *current = m_root;
+    while (current != nullptr) {
+      if (value > current->value) {
+        previous = current;
+        current = current->right;
+      } else if (value < current->value) {
+        previous = current;
+        current = current->left;
+      } else {
+        if (current->left == nullptr && current->right == nullptr) {
+          if (previous == nullptr) {
+            m_root = nullptr;
+          } else if (previous->right == current) {
+            previous->right = nullptr;
+          } else {
+            previous->left = nullptr;
+          }
+          delete current;
+          return;
+        } else if (current->left != nullptr && current->right == nullptr) {
+          if (previous == nullptr) {
+            m_root = current->left;
+          } else if (previous->right == current) {
+            previous->right = current->left;
+          } else {
+            previous->left = current->left;
+          }
+          delete current;
+          return;
+        } else if (current->left == nullptr && current->right != nullptr) {
+          if (previous == nullptr) {
+            m_root = current->right;
+          } else if (previous->right == current) {
+            previous->right = current->right;
+          } else {
+            previous->left = current->right;
+          }
+          delete current;
+          return;
+        } else {
+          Node *successorParent = current;
+          Node *successor = current->right;
+          while (successor->left != nullptr) {
+            successorParent = successor;
+            successor = successor->left;
+          }
+          current->value = successor->value;
+          if (successorParent->left == successor) {
+            successorParent->left = successor->right;
+          } else {
+            successorParent->right = successor->right;
+          }
+          delete successor;
+          return;
+        }
+      }
+    }
+    std::cout << "Can't remove value that is not in BST.\n";
+  }
 };
 
 int main() {
